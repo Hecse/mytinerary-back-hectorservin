ffc740ca9bc89a8c8575e5bb625fe672e0e13b7b
Hector <hecse71@gmail.com>
2023-08-14 15:45:20 -0300

    corrigiendo git

---
+++ controllers/user.controller.js
@@ -0,0 +1,12 @@
+const controller = {
+    getUsers: (req, res) => {
+        res.json({
+            user: 'Hector Servin'
+        });
+    },
+
+    postUser: () => {},
+    deletetUser: () => {},
+}
+
+export default controller;
