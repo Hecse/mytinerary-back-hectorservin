6f7acc64a9214daed67f11bc76c4fbb5b0817c7e
Hector <hecse71@gmail.com>
2023-09-11 04:36:21 -0300

    se terminó la autenticación

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import express from "express";
-import crypto from 'crypto'
-import bcryptjs from 'bcrypt'
+import crypto from 'crypto';
+import bcryptjs from 'bcryptjs';
 import User from "../models/users.js";
 
 
@@ -15,11 +15,37 @@ const controller = {
             return res.status(201).json({
                 success: true,
                 message: 'Usuario registrado'
-            })            
+            })
         } catch (error) {
-            res.status(500)
+            next(error)
         }
-    } 
+    },
+
+    signin: async (req, res, next) => {
+        try {
+            let user = await User.findOneAndUpdate(
+                { email: req.user.email },
+                { online: true },
+                { new: true }
+            )
+
+            user.password = null;
+
+            return res.status(200).json({
+                succes: 'true',
+                message: 'Usuario logeado correctamente',
+                response: {
+                    name: user.name,
+                    lastname: user.lastname,
+                    email: user.email,
+                    image: user.image
+                }
+            })
+
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 export default controller;
--- router/auth.router.js
+++ router/auth.router.js
@@ -1,11 +1,22 @@
 import express from "express";
 import authController from '../controllers/auth.controller.js';
 import { accountExistSignup } from "../middlewares/auth/accountExistSignup.js";
+import { accountExistSignin } from "../middlewares/auth/accountExistSignin.js"
+import { accountHasBeenVerified } from "../middlewares/auth/accountHasBeenVerified.js"
+import {passwordIsOk} from '../middlewares/auth/passwordIsOk.js'
 
-const {signup} = authController
+const { signup, signin } = authController
 
 const router = express.Router();
 
-router.post('/signup', accountExistSignup, signup)
+router.post('/signup',
+    accountExistSignup,
+    signup)
+
+router.post('/signin',
+    accountExistSignin,
+    accountHasBeenVerified,
+    passwordIsOk,
+    signin)
 
 export default router;
